1bc5f78b56c9a003bcbbb2bb4324332f6389c27f
Dominic Barber <dominic.barber@infinityworks.com>
2022-03-28 13:44:25 +0100

    tax calculator refactoring

--- src/test/java/taxcalc/TaxCalcTest.java
+++ src/test/java/taxcalc/TaxCalcTest.java
@@ -9,18 +9,23 @@ public class TaxCalcTest {
 
     @Test
     public void canCalculateTax() throws Exception {
-        Integer first = new TaxCalc(10).netAmount(new TaxCalc.Pair<>(40, "GBP"), new TaxCalc.Pair<>(50, "GBP"), new TaxCalc.Pair<>(60, "GBP")).first;
+        Integer first = new TaxCalc(10).netAmount(new TaxCalc.Pair<>(40, "GBP"), new TaxCalc.Pair<>(50, "GBP"), new TaxCalc.Pair<>(60, "GBP")).amount;
         assertEquals(135, first.intValue());
+    }
+
+    @Test
+    public void canCalculateTaxNegative() throws Exception {
+        Integer first = new TaxCalc(10).netAmount(new TaxCalc.Pair<>(-40, "GBP"), new TaxCalc.Pair<>(50, "GBP"), new TaxCalc.Pair<>(60, "GBP")).amount;
+        assertEquals(63, first.intValue());
     }
 
     @Test
     public void cannotSumDifferentCurrencies() throws Exception {
         try {
-            new TaxCalc(10).netAmount(new TaxCalc.Pair<>(4, "GBP"),
-                    new TaxCalc.Pair<>(5, "USD"));
+            new TaxCalc(10).netAmount(new TaxCalc.Pair<>(4, "GBP"), new TaxCalc.Pair<>(5, "USD"));
             fail("didn't throw");
         } catch (Exception e) {
-
+            // empty catch block
         }
     }
 }
